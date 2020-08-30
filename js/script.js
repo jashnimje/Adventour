3ea74c50aa2bf10896f78113dee16036a596878f
Jash Nimje <jashnimje@gmail.com>
2020-08-30 15:29:16 +0530

    Added Booking Page

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
-var name = prompt("Enter Your Name: ");
-console.log(name);
+var currentDateTime = new Date();
+var year = currentDateTime.getFullYear();
+var month = currentDateTime.getMonth() + 1;
+var date = currentDateTime.getDate() + 1;
+
+if (date < 10) {
+  date = "0" + date;
+}
+if (month < 10) {
+  month = "0" + month;
+}
+
+var dateTomorrow = year + "-" + month + "-" + date;
+var dateElem = document.querySelector("#date-slot");
+
+dateElem.setAttribute("min", dateTomorrow);
+
+dateElem.onchange = function () {
+  checkoutElem.setAttribute("min", this.value);
+};
