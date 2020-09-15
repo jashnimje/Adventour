a58a7b1fcf354235380d05f299e880b6368c1437
Jash Nimje <jashnimje@gmail.com>
2020-09-15 10:35:36 +0530

    Removed Slideshow

--- js/script.js
+++ js/script.js
@@ -1,31 +1,8 @@
-// Slideshow
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides((slideIndex += n));
+// Navigation
+function openNav() {
+  document.getElementById("nav-bar").style.width = "250px";
 }
 
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+function closeNav() {
+  document.getElementById("nav-bar").style.width = "0";
 }
