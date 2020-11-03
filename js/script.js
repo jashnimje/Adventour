3e8e62279342b2d167f2ed7a8cebefaf973be295
Jash Nimje <jashnimje@gmail.com>
2020-11-03 13:49:41 +0530

    Login Content

--- js/script.js
+++ js/script.js
@@ -6,3 +6,21 @@ function openNav() {
 function closeNav() {
   document.getElementById("navbar").style.width = "0";
 }
+
+// Get the modal
+var modalLogin = document.getElementById("modal-login");
+var modalSignup = document.getElementById("modal-signup");
+var modalAccount = document.getElementById("modal-account");
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modalLogin) {
+    modalLogin.style.display = "none";
+  }
+  if (event.target == modalSignup) {
+    modalSignup.style.display = "none";
+  }
+  if (event.target == modalAccount) {
+    modalAccount.style.display = "none";
+  }
+};
