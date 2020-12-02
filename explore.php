041df2bcff954c1ba5b678ca86c692c7917ece0b
Jash Nimje <jashnimje@gmail.com>
2020-12-02 18:53:43 +0530

    Explore page

--- explore.php
+++ explore.php
@@ -1,13 +1,18 @@
 <html lang="en">
 
 <head>
-  <title>Adventour | Explore</title>
-  <?php include './components/header.php'; ?>
+    <title>Adventour | Explore</title>
+    <?php include './components/header.php'; ?>
 </head>
 
 <body id="explore">
-  <?php include './components/nav.php'; ?>
-  <?php include './components/footer.php'; ?>
+    <?php include './components/nav.php'; ?>
+    <section class="explore-home">
+        <div class="explore-content">
+            <h2>Dream<br>Trip</h2>
+        </div>
+    </section>
+    <?php include './components/footer.php'; ?>
 </body>
 
 </html>
